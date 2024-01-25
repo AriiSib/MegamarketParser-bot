06831f901b9f2129ab1bae2af1afd9a6ecd8a486
Artemy_Khokhlov <foxirt@mail.ru>
2024-01-25 19:43:07 +0600

    first

---
+++ src/main/java/com/example/MegamarketPrsBot/MegamarketPrsBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.MegamarketPrsBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegamarketPrsBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegamarketPrsBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MegamarketPrsBot/MegamarketPrsBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MegamarketPrsBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegamarketPrsBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
